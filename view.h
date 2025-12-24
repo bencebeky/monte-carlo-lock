b2c2e79db9a39b9540905d5007a6150079f97255
Bence Béky <zsebkecske@gmail.com>
2025-12-24 05:59:59 -0500

    s/Slice/View/

--- view.h
+++ view.h
@@ -2,15 +2,15 @@
 
 // View of a substring of an externally stored string, much like
 // std::string_view, but also supports reverse substrings.
-class Slice {
+class View {
  public:
   enum class Direction { kForward, kReverse };
 
-  // `data` points to the first element. If `direction` is forward, then Slice
+  // `data` points to the first element. If `direction` is forward, then View
   // points to memory range [data, data + length - 1]. If `direction` is
   // reverse, then points to memory range [data - lenght + 1, data], and
   // reverses the substring stored in that range.
-  Slice(char* data, size_t length, Direction direction = Direction::kForward)
+  View(char* data, size_t length, Direction direction = Direction::kForward)
       : data_(data), length_(length), direction_(direction) {}
 
  private:
