7656ed8bdcbd66c7204cbc710509bb80bd7ef680
Bence Béky <zsebkecske@gmail.com>
2025-12-24 07:11:13 -0500

    add length()

--- view.h
+++ view.h
@@ -5,7 +5,11 @@
 // std::string_view, but also supports reverse substrings.
 class View {
  public:
-  enum class Direction { kForward, kReverse };
+  // A signed size_type allows for seemless signed arithmetics.
+  using size_type int;
+  using value_type char;
+
+  enum class Direction { kForward = 1, kReverse = -1 };
 
   View() : data_(nullptr), length_(0), direction_(Direction::kForward) {}
 
@@ -13,18 +17,22 @@ class View {
   // points to memory range [data, data + length - 1]. If `direction` is
   // reverse, then points to memory range [data - lenght + 1, data], and
   // reverses the substring stored in that range.
-  View(char* data, size_t length, Direction direction = Direction::kForward)
+  View(value_type* data, size_type length,
+       Direction direction = Direction::kForward)
       : data_(data), length_(length), direction_(direction) {}
 
   // This allows conversion from std::string and C-style string (including
   // string literals) as well, courtesy of std::string_view's implicit
   // constructors.
-  View(std::string_view s) : data_(s.data()), length_(s.length()), direction_(Direction::kForward) {}
+  View(std::string_view s)
+      : data_(s.data()), length_(s.length()), direction_(Direction::kForward) {}
 
   bool empty() const { return length_ == 0; }
 
+  size_type length() const { return length_; }
+
  private:
-  const char* const data_;
-  const size_t length_;
+  const value_type* const data_;
+  const size_type length_;
   const Direction direction_;
 };
--- view_test.cc
+++ view_test.cc
@@ -3,7 +3,59 @@
 #include <gtest/gtest.h>
 
 TEST(View, Empty) {
-  View v;
+  {
+    View v;
+    EXPECT_TRUE(v.empty());
+    EXPECT_EQ(0, v.length());
+  }
 
-  EXPECT_TRUE(v.empty());
+  {
+    View v("");
+    EXPECT_TRUE(v.empty());
+    EXPECT_EQ(0, v.length());
+  }
+
+  {
+    std::string s;
+    View v(s);
+    EXPECT_TRUE(v.empty());
+    EXPECT_EQ(0, v.length());
+  }
+
+  {
+    View v(nullptr, 0);
+    EXPECT_TRUE(v.empty());
+    EXPECT_EQ(0, v.length());
+  }
+
+  {
+    View v(nullptr, 0, View::Direction::kReverse);
+    EXPECT_TRUE(v.empty());
+    EXPECT_EQ(0, v.length());
+  }
+}
+
+TEST(View, Length) {
+  {
+    View v("foo");
+    EXPECT_EQ(3, v.length());
+  }
+
+  {
+    std::string s("bar");
+    View v(s);
+    EXPECT_EQ(3, v.length());
+  }
+
+  {
+    std::string s("foobar");
+    View v(s.data(), s.length());
+    EXPECT_EQ(6, v.length());
+  }
+
+  {
+    std::string s("foobar");
+    View v(s.data(), s.length(), View::Direction::kReverse);
+    EXPECT_EQ(6, v.length());
+  }
 }
