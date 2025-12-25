64b386c0e4a5a12df8f118ed4d47b08ea4d0104f
Bence Béky <zsebkecske@gmail.com>
2025-12-25 17:26:46 -0400

    Wrap memoization in RelationshipCache class

--- memoize.cc
+++ memoize.cc
@@ -2,46 +2,56 @@
 #include "related.h"
 #include "view.h"
 
+#include <functional>
 #include <iostream>
 #include <set>
 #include <string>
 
-// TODO Look up how to partially bind class instance to method and package
-// these in a class.
-std::set<std::pair<std::string, std::string>> related_cache;
-std::set<std::pair<std::string, std::string>> not_related_cache;
+class RelationshipCache {
+ public:
+  bool related(View first, View second) {
+    const related_type bound_related =
+        std::bind(&RelationshipCache::related, this, std::placeholders::_1,
+                  std::placeholders::_2);
 
-bool related(View first, View second) {
-  std::pair<std::string, std::string> pair(first.ToString(), second.ToString());
+    std::pair<std::string, std::string> pair(first.ToString(),
+                                             second.ToString());
 
-  if (related_cache.find(pair) != related_cache.end()) {
-    return true;
+    if (related_cache_.find(pair) != related_cache_.end()) {
+      return true;
+    }
+
+    if (not_related_cache_.find(pair) != not_related_cache_.end()) {
+      return false;
+    }
+
+    const bool is_related = Q_related(first, second, bound_related) ||
+                            L_related(first, second, bound_related) ||
+                            V_related(first, second, bound_related) ||
+                            R_related(first, second, bound_related);
+
+    if (is_related) {
+      related_cache_.insert(pair);
+    } else {
+      not_related_cache_.insert(pair);
+    }
+
+    return is_related;
   }
 
-  if (not_related_cache.find(pair) != not_related_cache.end()) {
-    return false;
-  }
-
-  const bool is_related =
-      Q_related(first, second, related) || L_related(first, second, related) ||
-      V_related(first, second, related) || R_related(first, second, related);
-
-  if (is_related) {
-    related_cache.insert(pair);
-  } else {
-    not_related_cache.insert(pair);
-  }
-
-  return is_related;
-}
+ private:
+  std::set<std::pair<std::string, std::string>> related_cache_;
+  std::set<std::pair<std::string, std::string>> not_related_cache_;
+};
 
 int main() {
   const int kMaxLength = 10;
+  RelationshipCache r;
 
   for (int length = 1; length <= kMaxLength; length++) {
     std::string combination = first_combination(length);
     do {
-      if (related(View(combination), View(combination))) {
+      if (r.related(View(combination), View(combination))) {
         std::cout << combination << std::endl;
       }
     } while (next_combination(combination));
